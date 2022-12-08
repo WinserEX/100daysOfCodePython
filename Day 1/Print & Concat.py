35e32dd9413f5cd78d90bd6ce8ddc726cb4aa08e
WinserEX <winser500@gmail.com>
2022-12-07 23:12:20 -0400

    Added Input function practice

---
+++ Day 1/Print & Concat.py
@@ -0,0 +1,9 @@
+#Write your code here
+
+print("Hello")
+print("Hello" + " " + "Winser")
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
