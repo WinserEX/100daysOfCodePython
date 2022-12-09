96401106fbcfd61cdd66f6e7d92bd8cc5d57dffa
Winser Espinal <Wespinal@vivaresorts.com>
2022-12-09 12:10:54 -0400

    Completed data types exercise

---
+++ Day 2/Primitives.py
@@ -0,0 +1,29 @@
+#Data Types
+
+#String
+
+"Hello"
+
+print("Hello"[4])
+
+"123"
+
+print("123" + "345")
+
+#integer
+
+123
+
+print(123 + 345)
+
+3.14 # -> Float
+
+90_90  
+
+9090
+
+#Boolean
+
+True
+
+False
---
+++ Day 2/typeCheck.py
@@ -0,0 +1,8 @@
+num_char = len(input("What is your name? "))
+print("your name has " + str(num_char), " characters")
+
+print(type(num_char))
+
+float(123)
+
+int("123")
