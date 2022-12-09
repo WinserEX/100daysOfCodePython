153c812d1ea1ff9cb8e88251de7ad133a7d78c4a
Winser Espinal <Wespinal@vivaresorts.com>
2022-12-09 14:50:57 -0400

    Completed age in weeks calculator

---
+++ Day 2/lifeInWeeks_exercise.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+whatIsLeft = 90 - int(age)
+
+print(f"You have {whatIsLeft * 365} days, {whatIsLeft * 52} weeks, and {whatIsLeft * 12} months left")
+
+#You have 12410 days, 1768 weeks, and 408 months left.
+
+#There are 365 days in a year, 52 weeks in a year and 12 months in a year.
