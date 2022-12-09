fc14d346b1f48364b58cb1ddd59fe3422666bd1f
Winser Espinal <Wespinal@vivaresorts.com>
2022-12-09 14:03:57 -0400

    Completed BMI calculator

---
+++ Day 2/BMIcalculator.py
@@ -0,0 +1,12 @@
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+# bmi = weight(kg)/Height(m)**2
+#Winser = 1.93m 99.79kg
+bmi = float(weight)/float(height)**2
+
+print(int(bmi))
---
+++ Day 2/mathOperations.py
@@ -0,0 +1,19 @@
+3 + 5
+7 - 4
+3 * 2
+6 / 3 #always gives you a float
+2 ** 3
+
+print(3 + 5)
+print(7 - 4)
+print(3 * 2)
+print(6 / 3) #always gives you a float
+print(2 ** 3)
+
+#PEMDAS
+# ()
+# **
+# * /
+# + -
+
+print(3 * (3 + 3) / 3 - 3) 
