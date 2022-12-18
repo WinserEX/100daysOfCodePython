6319d729c8cfd6d94d30d625864042f8083ddb54
WinserEX <winser500@gmail.com>
2022-12-18 11:05:47 -0400

    Will add solution to Banker Roulette exercise

---
+++ Day 4/BankerRoulette.py
@@ -0,0 +1,13 @@
+# Import the random module here
+import random
+# Split string method
+names_string = input("Give me everybody's names, separated by a comma. ")
+names = names_string.split(", ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+length = len(names)
+
+randNum = random.randint(1,length)
+
+print(f"{names[randNum]} is going to buy the meal today!")
