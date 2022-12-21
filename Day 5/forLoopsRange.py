d869309f334853476e98d07fbd463a672ab569f0
WinserEX <winser500@gmail.com>
2022-12-21 00:29:27 -0400

    Will add solution to Adding Even Numbers Exercise

---
+++ Day 5/forLoopsRange.py
@@ -0,0 +1,11 @@
+#(Range, Range -1, Steps)
+for number in range(1, 11, 3):
+    print(number)
+
+
+#(Range, Range -1) This will sum all # within range.
+total = 0
+for number in range(1, 101):
+    total += number
+print(total)
+
